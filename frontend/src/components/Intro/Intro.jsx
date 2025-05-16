f3d529cbebb116eebe25b26dd8cdce28f7f9ed4f
Vishal8700 <vishalkumar09837@gmail.com>
2025-05-16 23:54:51 +0530

    add

--- frontend/src/components/Intro/Intro.jsx
+++ frontend/src/components/Intro/Intro.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import React from 'react';
-import './intro.css';
+import './Intro.css';
 import { useNavigate } from 'react-router-dom';
 // SVG Components
 import { Link } from 'react-router-dom';
