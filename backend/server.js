964fd031b1f49b421dd851c80f9fce6cdaa3c032
Vishal8700 <vishalkumar09837@gmail.com>
2025-05-17 00:15:50 +0530

    update-backend-add-cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://todoist-nsut.vercel.app'],
     credentials: true
 }));
 
