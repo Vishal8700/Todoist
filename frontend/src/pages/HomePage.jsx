e8fad3494ac5638401c0e7ad852c89384320daae
Vishal8700 <vishalkumar09837@gmail.com>
2025-05-16 23:51:21 +0530

    add

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import TaskForm from '../components/TaskForm/TaskForm';
 import TaskFilter from '../components/TaskFilter/TaskFilter';
 import Calendar from '../components/Calendar/Calendar';
 import MeetingForm from '../components/MeetingForm/MeetingForm';
-import Footer from '../components/Footer/Footer.jsx'; // Corrected import path to match file system casing
+import Footer from '../components/footer/Footer.jsx'; // Corrected import path to match file system casing
 import Modal from '../components/Modal/Modal';
 import { generateId } from '../utils/helpers';
 import './HomePage.css';
